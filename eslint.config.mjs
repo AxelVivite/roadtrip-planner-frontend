72c22ef733d578ba4da8fc259e0203b5dd7bc414
AVirot <axel.virot@gmail.com>
2025-09-28 22:22:36 +0200

    feat: Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import unicorn from "eslint-plugin-unicorn";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  js.configs.recommended,
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    plugins: { unicorn },
+    rules: {
+      ...unicorn.configs.recommended.rules,
+    },
+  },
+  {
+    ignores: [
+      "node_modules/**",
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts"
+    ],
+  },
+];
+
+export default eslintConfig
